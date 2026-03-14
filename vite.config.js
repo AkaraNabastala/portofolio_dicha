f06bace3d3e4e30c94eb25a9fe8582c75beed0a5
astrophile <149647063+AkaraNabastala@users.noreply.github.com>
2026-03-14 20:34:12 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/porto_dicha/',
+  base: '/portofolio_dicha/',
 })
